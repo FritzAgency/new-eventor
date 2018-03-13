f2d759ecb41fbb4b98a12b9a7688907e91c5cb64
charmming <charmming4u@gmail.com>
2018-03-13 19:03:58 +0100

    sending the login page

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,7 @@ create event page
 
 // Route::get('/', 'CreateEventController@display'); 
 
-//This route is for signup page
-Route::get('/signup', 'PagesController@signup');
 
-<<<<<<< HEAD
-//This route is for login page
-Route::get('/login', 'PagesController@login');
-=======
 Route::post('/signup', 'SignupController@register'); 
->>>>>>> 0b48bd9c76e20fef3904283b37e4f3367eca96b6
+
+Route::post('/signup', 'LoginController@login'); 
