70d8832f91d30aeea7aa0120e75e139425a16405
charmming <charmming4u@gmail.com>
2018-03-16 11:03:07 +0100

    adding create event form

--- resources/views/event/createevent.blade.php
+++ resources/views/event/createevent.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -14,25 +15,25 @@
         
         <div class="container">
                 <div class="row">
-                        <div class="col-sm-8"
+                        <div class="col-sm-12"
                         <form class="form-horizontal" role="form">
                                         <h2>Create Event</h2>
                                         <div class="form-group">
                                             
-                                            <div class="col-sm-7">
+                                            <div class="col-sm-12">
                                                 <label for="firstName" class="col-sm-5 control-label" style="text-align:left;">Event Name</label>
                                                 <input type="text" id="firstName" placeholder="Name Of Event" class="form-control" autofocus>
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                                <div class="col-sm-7">
+                                                <div class="col-sm-12">
                                                         <label for="description" class="col-sm-5 control-label" style="text-align:left;">Description</label>
                                                         <textarea class="form-control" rows="5" id="comment" placeholder="Write a short note to descripe your event"></textarea>
                                                 </div>
                                         </div>
 
                                         <div class="form-group">
-                                                <div class="col-sm-7">
+                                                <div class="col-sm-12">
                                                         <label class="radio-inline">
                                                         <input type="radio" name="optradio">Free
                                                         </label>
@@ -42,7 +43,7 @@
                                                 </div>
                                         </div>
                                         <div class="form-group">
-                                                <div class="col-sm-7">
+                                                <div class="col-sm-12">
                                         <ul class="nav navbar-nav">
                                                         <li class="dropdown">
                                                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sponsors <b class="caret"></b></a>
@@ -59,7 +60,7 @@
                                                 </div>
                                         </div>
                                         <div class="form-group">
-                                                        <div class="col-sm-7">
+                                                        <div class="col-sm-12">
                                                             <label for="orgName" class="col-sm-12 control-label" style="text-align:left;">Organizer Name</label>
                                                             <input type="text" id="OrgName" placeholder="Organizer Name" class="form-control">
                                                         </div>
@@ -67,68 +68,76 @@
                                                     <div class="form-group">
                                                                 <div class="col-md-12">
                                                                                 <form method="post" action="#" id="#">
-                                                                             
-                                                                                
-                                                                                
-                                                                                
+  
                                                                                 <div class="form-group files color">
                                                                                   <label>Attach Event Logo </label>
                                                                                   <input type="file" class="form-control" multiple="">
                                                                                 </div>
-                                                                                
-                                                                              
                                                                             </form>
-                                                                                
-                                                                                
                                                                             </div>
                                                         </div>
 
                                                         <div class="form-group">
-                                                                        <div class="col-md-12">
-                                                                                        <form method="post" action="#" id="#">
-                                                                                     
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                                        <div class="form-group files color">
-                                                                                          <label>Attach Organizer Logo </label>
-                                                                                          <input type="file" class="form-control" multiple="">
-                                                                                        </div>
-                                                                                        
-                                                                                      
-                                                                                    </form>
-                                                                                        
-                                                                                        
-                                                                                    </div>
+                                                                <div class="col-md-12">
+                                                                        <form method="post" action="#" id="#">
+   
+                                                                <div class="form-group files color">
+                                                                        <label>Attach Organizer Logo </label>
+                                                                        <input type="file" class="form-control" multiple="">
                                                                 </div>
-                                        <div class="form-group">
-                                             <div class="col-sm-7">
+                                                                </form>
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                                        <div class="col-sm-12">
+                                                                            <label for="Org Address" class="col-sm-12 control-label" style="text-align:left;">Organizer Address</label>
+                                                                            <input type="text" id="OrgAddress" placeholder="Enter Address" class="form-control">
+                                                                        </div>
+                                                                    </div>
+                                                        <div class="form-group">
+                                                                        <div class="row">
+                                                                        <div class="col-sm-6">
+                                                                        <label for="city" class="col-sm-5 control-label" style="text-align:left;">City</label>
+                                                                        <select id="city" class="form-control" placeholder="choose city">
+                                                                                <option>Choose City</option>
+                                                                                <option>Ikorodun</option>
+                                                                                <option>Mushin</option>
+                                                                                <option>Egbeda</option>
+                                                                                <option>Lekki</option>
+                                                                                <option>Fiji</option>
+                                                                                <option>Swoww</option>
+                                                                                <option>Haitoi</option>
+                                                                        </select>
+                                                                        </div>
+                                                                 <div class="col-sm-6">
+                                                                        <label for="lga" class="col-sm-5 control-label" style="text-align:left;">LGA</label>
+                                                                        <select id="LGA" class="form-control">
+                                                                            <option>Choose LGA</option>
+                                                                            <option>Alimosho</option>
+                                                                            <option>Egbeda</option>
+                                                                            <option>Eko</option>
+                                                                            <option>Beeni</option>
+                                                                            <option>Oniru</option>
+                                                                            <option>Ijesah</option>
+                                                                            <option>Here</option>
+                                                                        </select>
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                        <i class="material-icons">&#xe567;</i>
+                                <div class="form-group">
+                                             <div class="col-sm-12">
                                                 <label for="password" class="col-sm-5 control-label" style="text-align:left;">Password</label>
                                                 <input type="password" id="password" placeholder="Password" class="form-control">
                                             </div>
                                         </div>
                                         <div class="form-group">
-                                        <div class="col-sm-7">
+                                        <div class="col-sm-12">
                                                         <label for="birthDate" class="col-sm-5 control-label" style="text-align:left;">Date of Birth</label>
                                                 <input type="date" id="birthDate" class="form-control">
                                             </div>
                                         </div>
-                                        <div class="form-group">
-                                            
-                                            <div class="col-sm-7">
-                                                <label for="country" class="col-sm-5 control-label" style="text-align:left;">Country</label>
-                                                <select id="country" class="form-control">
-                                                    <option>Afghanistan</option>
-                                                    <option>Bahamas</option>
-                                                    <option>Cambodia</option>
-                                                    <option>Denmark</option>
-                                                    <option>Ecuador</option>
-                                                    <option>Fiji</option>
-                                                    <option>Gabon</option>
-                                                    <option>Haiti</option>
-                                                </select>
-                                            </div>
-                                        </div> 
+                                        
                                         <div class="form-group">
                                             <label class="control-label col-sm-3">Gender</label>
                                             <div class="col-sm-6">
